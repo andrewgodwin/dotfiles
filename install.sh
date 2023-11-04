0f79bf75ccf06aabbc3ba66db7670ef9fe17a63e
Andrew Godwin <andrew@aeracode.org>
2023-11-04 16:22:49 -0600

    Add screen config

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Link dotfiles
+rm $HOME/.bashrc
 ln -s $SCRIPT_DIR/.bashrc $HOME/.bashrc
 ln -s $SCRIPT_DIR/.gitconfig $HOME/.gitconfig
 ln -s $SCRIPT_DIR/.gitexclude $HOME/.gitexclude
+ln -s $SCRIPT_DIR/.screenrc $HOME/.screenrc
