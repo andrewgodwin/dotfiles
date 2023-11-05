8676d888d083f32646f36176360642a8fa3b704a
Andrew Godwin <andrew@aeracode.org>
2023-11-04 18:22:57 -0600

    Add pyenv install

--- install.sh
+++ install.sh
@@ -8,3 +8,11 @@ ln -sf $SCRIPT_DIR/.bashrc $HOME/.bashrc
 ln -sf $SCRIPT_DIR/.gitconfig $HOME/.gitconfig
 ln -sf $SCRIPT_DIR/.gitexclude $HOME/.gitexclude
 ln -sf $SCRIPT_DIR/.screenrc $HOME/.screenrc
+
+# Install pyenv and friends
+if [ ! -d $HOME/.pyenv ]; then
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+fi
+if [ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]; then
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+fi
