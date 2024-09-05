eb993e3bfeb413b9fc4fe4340c4729e23947e38b
Andrew Godwin <andrew@aeracode.org>
2024-09-05 16:39:21 -0600

    Add keychain to install

--- install.sh
+++ install.sh
@@ -10,20 +10,12 @@ ln -sf $SCRIPT_DIR/.gitexclude $HOME/.gitexclude
 ln -sf $SCRIPT_DIR/.screenrc $HOME/.screenrc
 ln -sf $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
 
-# Install pyenv and friends
-if [ ! -d $HOME/.pyenv ]; then
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-    eval "$(pyenv init -)"
-fi
-if [ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]; then
-    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-fi
-
 # Install common packages
 sudo apt-get update
 sudo apt-get install -y build-essential \
     tmux \
     htop \
+    keychain \
     python3-pip \
     python3-dev \
     python3-venv \
@@ -34,3 +26,12 @@ sudo apt-get install -y build-essential \
     libjpeg-dev \
     libpng-dev \
     libffi-dev
+
+# Install pyenv and friends
+if [ ! -d $HOME/.pyenv ]; then
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    eval "$(pyenv init -)"
+fi
+if [ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]; then
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+fi
